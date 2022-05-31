94079b69467ee5af87fdb21eed509340c10ad679
cscristian <welcome_salta@hotmail.com>
2022-05-31 14:15:20 -0300

    5h Commit

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ const App = () => {
     return(
         <HashRouter>
             <Routes>
-                <Route path={process.env.PUBLIC_URL + "/"} element={<Home/>}/>
-                <Route path={process.env.PUBLIC_URL + "/acerca_de_la_tartamudez"} element={<Stuttering/>}/>
+                <Route path={process.env.PUBLIC_URL + "/#"} element={<Home/>}/>
+                <Route path={process.env.PUBLIC_URL + "/#/acerca_de_la_tartamudez"} element={<Stuttering/>}/>
             </Routes>
         </HashRouter>
     );
